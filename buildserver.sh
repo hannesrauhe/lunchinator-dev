de440c74bf3f7e0b7b4e57da4732e84dbb3a9c98
Hannes Rauhe <hannes@scitivity.net>
2014-05-18 11:58:15 +0200

    buildserver writes hashes per command

--- buildserver.sh
+++ buildserver.sh
@@ -48,9 +48,9 @@ branches=(master nightly)
 for branch in "${branches[@]}"
 do
   LAST_HASH="HEAD^"
-  if [ -e last_hash_${branch} ]
+  if [ -e last_hash_${1}_${branch} ]
   then
-    LAST_HASH=$(cat last_hash_${branch})
+    LAST_HASH=$(cat last_hash_${1}_${branch})
   fi
   
   pushd "$LUNCHINATOR_GIT"
@@ -71,7 +71,7 @@ do
     if [ ${PIPESTATUS[0]} -eq 0 ]
     then
       log "Successfully built version $VERSION"
-      echo $THIS_HASH > last_hash_${branch}
+      echo $THIS_HASH > last_hash_${1}_${branch}
     fi
     log "Cleaning up"
     eval "./$1 --clean" 2>&1 | tee -a buildserver.log
